7805e1c11fc97d67ae30cb491ce1d8e3e22580c4
Ronak Malik <ronakmalik4242@gmail.com>
2019-07-14 21:43:49 -0400

    Fill in sigmoid function

--- machine-learning/machine-learning-ex2/ex2/sigmoid.m
+++ machine-learning/machine-learning-ex2/ex2/sigmoid.m
@@ -9,8 +9,15 @@ g = zeros(size(z));
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
+[m, n] = size(z);
 
+for i=1:m,
+  for j=1:n,
+      g(i, j) = 1 / (1 + e^(-z(i, j)));
+  endfor
+endfor
 
+%g = 1 ./ (1 + e.^(-z));
 
 
 % =============================================================
